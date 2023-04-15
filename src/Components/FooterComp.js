e8c7e5430f0eeb185b72ba62c4293ba871a51bb1
Sabuj Golui <83773953+SabujGolui@users.noreply.github.com>
2023-04-15 22:03:58 +0530

    addded a bu=it style

--- src/Components/FooterComp.js
+++ src/Components/FooterComp.js
@@ -1,22 +1,25 @@
 import React from "react";
-import { AiFillGithub } from 'react-icons/ai';
+import { AiFillGithub } from "react-icons/ai";
 
-function FooterComp() {
+function Footer() {
   return (
-    <div>
-      <footer className="footer fixed-bottom bg-light text-center text-lg-start">
-        <div
-          className="text-center p-4 "
-          style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+    <footer className="footer fixed-bottom bg-light text-center text-lg-start">
+      <div
+        className="text-center p-4"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        Made by Sabuj Golui. Get connected with me on GitHub:&nbsp;
+        <a
+          className="text-dark"
+          href="https://github.com/sabujgolui"
+          rel="noopener noreferrer"
+          target="_blank"
         >
-          Made by Sabuj Golui. Get connected with me on GitHub&nbsp;: &nbsp;
-          <a class="text-dark" href="https://mdbootstrap.com/">
-          <AiFillGithub/>
-          </a>
-        </div>
-      </footer>
-    </div>
+          <AiFillGithub />
+        </a>
+      </div>
+    </footer>
   );
 }
 
-export default FooterComp;
+export default Footer;
